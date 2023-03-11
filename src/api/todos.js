8e2cc0c0934fa3c757713b35682d3d85f6913606
LJBL22 <yourgitemail@example.com>
2023-03-11 21:43:40 +0800

    feat: add update & delete todo api

--- src/api/todos.js
+++ src/api/todos.js
@@ -11,8 +11,8 @@ export const getTodos = async () => {
   }
 };
 export const createTodo = async (payload) => {
+  const { title, isDone } = payload;
   try {
-    const { title, isDone } = payload;
     const res = await axios.post(`${baseUrl}/todos`, {
       title,
       isDone,
@@ -22,5 +22,23 @@ export const createTodo = async (payload) => {
     console.error('[Create Todos failed]:', error);
   }
 };
-export const patchTodo = () => {};
-export const deleteTodo = () => {};
+export const patchTodo = async (payload) => {
+  const { id, title, isDone } = payload;
+  try {
+    const res = await axios.patch(`${baseUrl}/todos/${id}`, {
+      title,
+      isDone,
+    });
+    return res.data;
+  } catch (error) {
+    console.error('[Patch Todo failed]:', error);
+  }
+};
+export const deleteTodo = async (id) => {
+  try {
+    const res = await axios.delete(`${baseUrl}/todos/${id}`);
+    return res.data;
+  } catch (error) {
+    console.error('[Delete Todo failed]:', error);
+  }
+};
