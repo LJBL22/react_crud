319095da71f0cc3e739fdcebc8a17fa2e039e041
LJBL22 <yourgitemail@example.com>
2023-03-14 14:05:11 +0800

    feat: add home page navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,19 @@
+import { useAuth } from 'contexts/AuthContext';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/todos');
+    } else {
+      navigate('/login');
+    }
+  }, [isAuthenticated, navigate]);
+  // 因為會依照狀態直接導引至其他頁面了，i.e. 不需要 homepage 了，所以可直接刪除此行
+  // return <div>HomePage</div>;
 };
 
 export default HomePage;
